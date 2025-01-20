ef18674bfaa31ab235dfa3f72f8076ce77bc6c9b
Monter-Projects <56485760+Monter-Projects@users.noreply.github.com>
2025-01-20 19:53:44 +0530

    Added screen-shot capture logic

--- TestCases/Test_login.py
+++ TestCases/Test_login.py
@@ -31,11 +31,13 @@ class Test_001_login:
         self.lp.login_click()
         self.lp.escape()
         login_act_title = self.driver.title
-        self.driver.close()
         if login_act_title == "Swag Labs":
+            self.driver.close()
             print("Title is matching")
             assert True
         else:
+            self.driver.save_screenshot(".\\Screenshots\\"+ "test_login.png")
+            self.driver.close()
             print("Title is not matching")
             assert False
 
@@ -49,31 +51,17 @@ class Test_001_login:
         self.lp.logout_click()
         self.lp.logout_link()
         logout_act_title = self.driver.title
-        self.driver.close()
         if logout_act_title == "Swag Labs":
-            print("Title is matching")
+            print("logout_Title is matching")
+            self.driver.close()
             assert True
         else:
-            print("Title is not matching")
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_logout.png")
+            self.driver.close()
+            print("Logout_Title is not matching")
             assert False
 
-    def test_logout1(self,setup):
-        self.driver = setup
-        self.driver.get(self.baseurl)
-        self.lp = Login(self.driver)
-        self.lp.set_username(self.username)
-        self.lp.set_password(self.password)
-        self.lp.login_click()
-        self.lp.logout_click()
-        self.lp.logout_link()
-        logout_act_title = self.driver.title
-        self.driver.close()
-        if logout_act_title == "Swag Labs":
-            print("Title is matching")
-            assert True
-        else:
-            print("Title is not matching")
-            assert True
+
 
 
 
