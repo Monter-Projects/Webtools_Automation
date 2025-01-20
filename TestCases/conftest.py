8d1ef29e80859260a77d2f65a20190a88fe4ea84
Monter-Projects <56485760+Monter-Projects@users.noreply.github.com>
2025-01-20 21:46:32 +0530

    Added multibrowser logic

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -2,8 +2,24 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
+def setup(browser):
+    if browser == 'Ie':
+        driver = webdriver.Ie()
+        driver.implicitly_wait(5)
+    elif browser == 'Firefox':
+        driver = webdriver.Firefox()
+        driver.implicitly_wait(5)
+    else:
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(5)
     return driver
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
+
 
