f77f7af175beb1ba2e28370047292c938294cc62
Monter-Projects <56485760+Monter-Projects@users.noreply.github.com>
2025-01-26 21:47:31 +0530

    updated logger information

--- Utilites/CustomLogger.py
+++ Utilites/CustomLogger.py
@@ -1,10 +1,14 @@
 import logging
+import os
 
 class LogGen:
     @staticmethod
     def loggen():
+        current_directory = os.path.dirname(__file__)
+        log_file = os.path.join(current_directory, 'automation.log')
         logger = logging.getLogger()
-        fhandler = logging.FileHandler(filename='.\\Logs\\automation.log', mode='a')
+        fhandler = logging.FileHandler(log_file, mode='a')
+        #fhandler = logging.FileHandler(filename='.\\Logs\\automation.log', mode='a')
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         fhandler.setFormatter(formatter)
         logger.addHandler(fhandler)
