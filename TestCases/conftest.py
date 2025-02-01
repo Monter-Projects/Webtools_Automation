6cb90ec3b11496fc42c2a68972766b55c936a115
Monter-Projects <56485760+Monter-Projects@users.noreply.github.com>
2025-02-01 20:14:09 +0530

    Added chrome headless

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = Options()
-#chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--headless")  # Run in headless mode
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")  # Ensure a full-screen size
 chrome_options.add_argument("--disable-dev-shm-usage")
